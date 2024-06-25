ea692c6e2d8f4cbd1ea94e2cad9f7b384de5e7eb
jeonij99 <ijjeon9912@gmail.com>
2024-06-25 17:23:01 +0900

    Create Area.cs

---
+++ CSharpBasic2/Area.cs
@@ -0,0 +1,20 @@
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. 변수선언 및 입력부
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+           
+            //2. 알고리즘 숫식
+            int result = (width* height)/; // 수식
+
+            //3. 출력부
+            Console.WriteLine($"sjfqdlsms{result}입니다.");
+
+        }
+    }
+}
