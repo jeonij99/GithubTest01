ff9ffbf0e260091560448a6cec6fa385f5b585ed
jeonij99 <ijjeon9912@gmail.com>
2024-06-25 17:23:50 +0900

    Add files via upload

---
+++ TriangleArea.cs
@@ -0,0 +1,21 @@
+
+namespace ConsoleApp9
+{
+    internal class TriangleArea
+    {
+        static void Main(string[] args)
+        {
+            //1. 변수선언 및 입력부
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+           
+            //2. 알고리즘 숫식
+            int result = (width* height)/2.0; // 수식
+
+            //3. 출력부
+            Console.WriteLine($"sjfqdlsms{result}입니다.");
+
+        }
+    }
+}
+
